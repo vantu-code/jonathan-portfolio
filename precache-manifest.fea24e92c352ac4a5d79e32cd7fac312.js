f3a4509b83fadc98d7f251a0f5f1d850349f98f3
Jonathan <jonathanvantu@gmail.com>
2019-12-27 22:50:55 +0100

    Updates

---
+++ precache-manifest.fea24e92c352ac4a5d79e32cd7fac312.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f5c1665513162fc73420f436f3fe3fd",
+    "url": "/jonathan-portfolio/index.html"
+  },
+  {
+    "revision": "edfb2984b7ce5c37b924",
+    "url": "/jonathan-portfolio/static/css/2.b107a2aa.chunk.css"
+  },
+  {
+    "revision": "125804ec8084688df10e",
+    "url": "/jonathan-portfolio/static/css/main.9d834f18.chunk.css"
+  },
+  {
+    "revision": "edfb2984b7ce5c37b924",
+    "url": "/jonathan-portfolio/static/js/2.e02e0502.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/jonathan-portfolio/static/js/2.e02e0502.chunk.js.LICENSE"
+  },
+  {
+    "revision": "125804ec8084688df10e",
+    "url": "/jonathan-portfolio/static/js/main.ac957c06.chunk.js"
+  },
+  {
+    "revision": "c67655c7d7df7e2a765d",
+    "url": "/jonathan-portfolio/static/js/runtime-main.bb22d20d.js"
+  }
+]);
